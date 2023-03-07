add1994f7cce891da99ed0fcba5a02997360d25f
chasj0326@naver.com <62418379+chasj0326@users.noreply.github.com>
2023-03-07 23:44:33 +0900

    feat: 입력 및 제출 기능 틀 구현

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,34 @@
+import {useState} from 'react';
+
 export default function Home() {
+  const [animalInput, setAnimalInput] = useState('');
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if(animalInput===''){
+      alert('please Input Your Animal!')
+      return;
+    }
+    console.log(animalInput);
+    setAnimalInput('');
+  }
   return (
     <>
       <div>
         <h3>Name My Pet!</h3>
-        <form>
+        <form onSubmit={handleSubmit}>
           <input
             type = 'text'
             name = 'animal'
+            value = {animalInput}
+            onChange = {(e)=>{
+              setAnimalInput(e.target.value);
+              // console.log(animalInput)
+            }}
             placeholder = 'Enter an animal'
           />
           <input
             type = 'submit'
-            value = 'submit'
+            value = 'Generate names'
           />
         </form>
       </div>
